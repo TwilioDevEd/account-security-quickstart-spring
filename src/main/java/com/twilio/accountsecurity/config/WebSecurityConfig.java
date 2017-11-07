f7dd60ab6877e16c28428c528075377805882d5a
Hector Ortega <hector.oj@gmail.com>
2017-11-07 08:47:21 -0500

    Use builtin login method

--- src/main/java/com/twilio/accountsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/twilio/accountsecurity/config/WebSecurityConfig.java
@@ -2,11 +2,14 @@ package com.twilio.accountsecurity.config;
 
 import com.twilio.accountsecurity.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -16,10 +19,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-            .antMatchers("/", "/api/**").permitAll()
-            .anyRequest().authenticated()
-                .and()
-            .formLogin().loginPage("/login").permitAll()
+                .antMatchers("/protected").authenticated()
+                .antMatchers("/", "/register/**", "/api/**", "/**.js", "/**.html").permitAll()
                 .and()
             .logout().permitAll();
     }
@@ -27,10 +28,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyUserDetailsService userDetailsService;
 
+    @Autowired
+    private PasswordEncoder encoder;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
         auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(authProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder);
+        return authProvider;
     }
 
 }
--- src/main/java/com/twilio/accountsecurity/controllers/RegisterController.java
+++ src/main/java/com/twilio/accountsecurity/controllers/RegisterController.java
@@ -8,16 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
@@ -29,29 +25,19 @@ public class RegisterController {
     @Autowired
     private RegisterService registerService;
 
-    @Autowired
-    protected AuthenticationManager authenticationManager;
-
     @RequestMapping(value = "/api/register",
             method = {RequestMethod.GET, RequestMethod.POST},
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity register(@Valid @RequestBody UserRegisterRequest newUserRequest, HttpServletRequest request) {
         try {
             registerService.register(newUserRequest);
-            authenticateUserAndSetSession(newUserRequest.getUsername(), newUserRequest.getPassword(), request);
+            request.login(newUserRequest.getUsername(), newUserRequest.getPassword());
             return ResponseEntity.ok().build();
         } catch (UserExistsException e) {
             return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).build();
+        } catch (ServletException e) {
+            LOGGER.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
-    }
-
-    private void authenticateUserAndSetSession(String username, String password, HttpServletRequest request) {
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
-
-        request.getSession();
-
-        token.setDetails(new WebAuthenticationDetails(request));
-        Authentication authenticatedUser = authenticationManager.authenticate(token);
-        SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
     }
 }
